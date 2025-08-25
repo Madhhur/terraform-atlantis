53d48b961457d1b747889aeafbe05a44ae08d719
Madhhur <mm7599355000@gmail.com>
2025-08-25 19:24:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_bucket" "example1" {
   bucket = "${var.bucket_name}"
   tags = {
     Name        = "atlantis-test-b11ucket"
