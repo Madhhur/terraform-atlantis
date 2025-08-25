c627c3c7a24266f3c05fb343b57f35f634091cb9
Madhhur <mm7599355000@gmail.com>
2025-08-25 23:31:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,13 +12,7 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "${var.bucket_name}"
-  tags = {
-    Name        = "atlantis-test-b11ucket"
-    Environment = "Dev"
-  }
-}
+
 
 terraform {
   backend "s3" {
